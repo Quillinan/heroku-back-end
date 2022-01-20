d6a870a109c5296415eb4caa410734ab62af9eaf
Quillinan <milton.quillinan1798@gmail.com>
2022-01-20 17:11:47 -0300

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as dotenv from "dotenv";
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from "express";
@@ -5,10 +6,12 @@ import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import routes from "./routes";
 
+dotenv.config();
+
 const app = express()
 createConnection()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
 
-app.listen(3333)
+app.listen(process.env.PORT || 3333)
