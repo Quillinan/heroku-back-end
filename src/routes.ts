791356fd11e7d4f35b96d432d28095f539f9efda
Quillinan <milton.quillinan1798@gmail.com>
2022-01-25 19:42:59 -0300

    pret

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,22 @@
-import {Router, Request, Response} from "express";
-import {getProdutos, saveProduto, getProduto, updateProduto, removeProduto} from './controller/ProdutosController';
+import { Router, Request, Response } from 'express';
+import {
+  getProdutos,
+  saveProduto,
+  getProduto,
+  updateProduto,
+  removeProduto,
+} from './controller/ProdutosController';
 
-const routes = Router()
+const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
-    return response.json({message: 'Server online'})
-})
+  return response.json({ message: 'Server online' });
+});
 
-routes.get('/produtos', getProdutos)
-routes.get('/produtos/:id', getProduto)
-routes.post('/produtos', saveProduto)
-routes.put('/produtos/:id', updateProduto)
-routes.delete('/produtos/:id', removeProduto)
+routes.get('/produtos', getProdutos);
+routes.get('/produtos/:id', getProduto);
+routes.post('/produtos', saveProduto);
+routes.put('/produtos/:id', updateProduto);
+routes.delete('/produtos/:id', removeProduto);
 
-export default routes
+export default routes;
