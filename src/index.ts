029610343e0b1ef1cba8cf0b4ab759feffee119a
Quillinan <milton.quillinan1798@gmail.com>
2022-01-26 11:28:38 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,18 @@ dotenv.config();
 
 const app = express();
 createConnection();
+const corsOptions = {
+  origin: 'http://localhost:3000/',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+  res.json({ msg: 'This is CORS-enabled for only example.com.' });
+});
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80');
+});
 app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
