9b057911fef558d93b864c9470d61da0b6fe9c76
Quillinan <39285144+Quillinan@users.noreply.github.com>
2022-01-26 13:24:47 -0300

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,6 @@
 console.log('process.env.DATABASE_URL :>>', process.env.DATABASE_URL);
 module.exports={
    "type": "postgres",
-   "url": process.env.DATABASE_URL,
    "synchronize": true,
    "logging": false,
    "entities": [
