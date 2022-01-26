e8acf365006c653b05a636a43f126781686008fd
Quillinan <milton.quillinan1798@gmail.com>
2022-01-26 12:55:05 -0300

    console

--- src/controller/ProdutosController.ts
+++ src/controller/ProdutosController.ts
@@ -1,53 +1,47 @@
-import {getRepository} from "typeorm";
-import { Request, Response} from "express";
-import {Produtos} from "../entity/Produtos";
-
+import { getRepository } from 'typeorm';
+import { Request, Response } from 'express';
+import { Produtos } from '../entity/Produtos';
 
 export const getProdutos = async (request: Request, response: Response) => {
-
-    const produtos = await getRepository(Produtos).find()
-    return response.json(produtos)
+  const produtos = await getRepository(Produtos).find();
+  return response.json(produtos);
 };
 
-export const getProduto = async(request: Request, response: Response) => {
-    const {id} = request.params
-    const produto = await getRepository(Produtos).findOne(id)
-    return response.json(produto)
+export const getProduto = async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const produto = await getRepository(Produtos).findOne(id);
+  return response.json(produto);
 };
 
-export const saveProduto = async(request: Request, response: Response) => {
-
-    const produtos = await getRepository(Produtos).save(request.body)
-    return response.json(produtos)
-
+export const saveProduto = async (request: Request, response: Response) => {
+  const produtos = await getRepository(Produtos).save(request.body);
+  return response.json(produtos);
 };
 
-export const updateProduto = async(request: Request, response: Response) => {
-    const {id} = request.params
+export const updateProduto = async (request: Request, response: Response) => {
+  const { id } = request.params;
+  console.log('Olá');
 
-    const produto = await getRepository(Produtos).update(id, request.body)
+  const produto = await getRepository(Produtos).update(id, request.body);
+  console.log('Tchau');
 
-    if (produto.affected == 1) {
-        const produtoUpdated = await getRepository(Produtos).findOne(id)
-        return response.json(produtoUpdated)
-    }
+  if (produto.affected == 1) {
+    const produtoUpdated = await getRepository(Produtos).findOne(id);
+    return response.json(produtoUpdated);
+  }
 
-    return response.status(404).json({message: "Produto não encontrado"})
-    
-
+  return response.status(404).json({ message: 'Produto não encontrado' });
 };
 
-export const removeProduto = async(request: Request, response: Response) => {
-    const {id} = request.params
+export const removeProduto = async (request: Request, response: Response) => {
+  const { id } = request.params;
 
-    const produto = await getRepository(Produtos).delete(id)
+  const produto = await getRepository(Produtos).delete(id);
 
-    if (produto.affected == 1) {
-        const produtoUpdated = await getRepository(Produtos).findOne(id)
-        return response.json({message: "Produto removido"})
-    }
+  if (produto.affected == 1) {
+    const produtoUpdated = await getRepository(Produtos).findOne(id);
+    return response.json({ message: 'Produto removido' });
+  }
 
-    return response.status(404).json({message: "Produto não encontrado"})
-    
-
+  return response.status(404).json({ message: 'Produto não encontrado' });
 };
--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,8 @@ import routes from './routes';
 
 dotenv.config();
 
-var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Content-Length, X-Requested-With'
-  );
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-
 const app = express();
 createConnection();
-app.use(allowCrossDomain);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
